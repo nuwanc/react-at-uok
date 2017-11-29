a134ec71491ba351994b72f1c52e1fa21d6d1b75
Nuwan Chandrasoma <nuwan.chandrasoma@gtnexus.com>
2017-11-29 18:13:17 +0530

    initial commit

---
+++ src/firestore.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+firebase.initializeApp({
+    "apiKey": "AIzaSyAzoGKs_Gk9qFbI4pv_zdUyolsjabNDd8M",
+    "databaseURL": "https://react-at-uok.firebaseio.com",
+    "storageBucket": "react-at-uok.appspot.com",
+    "authDomain": "react-at-uok.firebaseapp.com",
+    "messagingSenderId": "8870834897",
+    "projectId": "react-at-uok"
+});
+
+export default firebase.firestore();
